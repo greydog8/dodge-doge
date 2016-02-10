1d25cb16936c791983d6cb7b76681b9a37a82f47
Greyson <greydog8@skilstak.sh>
2016-02-10 13:14:13 -0500

    save

---
+++ www/js/play.js
@@ -0,0 +1,28 @@
+// State: dont die because of missles.Play
+
+'use strict'
+var DogeDodge = {};
+DogeDodge.Play = function () {}
+DogeDodge.Play.prototypeb = {
+  init function () {
+    console.log("%~~ Booting the Game ~~~\n Made By Greyson S")
+},
+
+
+ preload function () {
+
+ },
+
+
+ create function() {
+
+ },
+
+
+ update function() {
+
+ },
+
+
+};
+
